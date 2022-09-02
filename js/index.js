5be1a163ece6d2bb35cd2275653e540d6967b71b
Matypls <matyascaceres@hotmail.com>
2022-09-02 16:17:11 -0300

    desafio arrays

--- js/index.js
+++ js/index.js
@@ -1,42 +1,24 @@
-let impuesto = 1.75;
-let precio = 0;
-let resN = 0;
-let resS = 0;
-let resSt = 0;
-let ciclo = true
+let utiles = ['lapiz', 'goma', 'corrector', 'lapicera', 'colores', 'resaltador', 'rueda']
 
-debugger
-
-while(ciclo === true){
-    let producto = prompt("¿Que quieres cotizar Netflix, Spotify, Steam?").toLowerCase();
-
-    if (producto === "netflix"){
-        precio = parseFloat(prompt("Cuanto sale tu suscripcion?"));
-        resN = precio * impuesto;
-        console.log(`Tu suscripcion saldria: $${resN}`)
-        ciclo = confirm("¿Quieres seguir?")
-    }
-    else if (producto === "spotify"){
-        precio = parseFloat(prompt("Cuanto sale tu suscripcion?"));
-        resS = precio * impuesto;
-        console.log(`Tu suscripcion saldria: $${resS}`)
-        ciclo = confirm("¿Quieres seguir?")
-    }
-    else if (producto === "steam"){
-        precio = parseFloat(prompt("Cuanto sale tu juego?"));
-        resSt = precio * impuesto;
-        console.log(`Tu juego saldria: $${resSt}`)
-        ciclo = confirm("¿Quieres seguir?")
-    }
-    else{
-        alert("Fijate que escribiste mal.")
-    } 
+function agregarUtiles (){
+    let nuevosUtiles = prompt("Que util quieres agregar?")
+    let resultado = utiles.includes(nuevosUtiles)
+        if (resultado === false) {
+            utiles.push(nuevosUtiles)
+            console.table(utiles)
+        } else {
+            alert(`El util ${nuevosUtiles} ya esta en la lista`)
+        }
 }
 
-let final = confirm("¿Quiere el precio final?")
-
-precioFinal(resN, resS, resSt)
-
-function precioFinal(resN, resS, resSt){
-    alert(`Tu precio total seria: $${resN + resS + resSt}`)
+function quitarUtil (){
+    let restar = prompt("Que util quieres retirar?")
+    let retirar = utiles.indexOf(restar)
+    if (retirar > -1) {
+        let resultado = utiles.splice(retirar, 1)
+        console.log(resultado)
+        console.table(utiles)
+    } else {
+        alert(`El util ${restar} no se encuentra en la lista`)
+    }
 }
